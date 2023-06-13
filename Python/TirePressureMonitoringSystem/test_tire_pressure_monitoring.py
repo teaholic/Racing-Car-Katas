c8eb4443652092538624b050d1d5ea468d9caac2
Chiara Barbagianni <chiara.barbagianni@agilelab.it>
2023-06-13 16:23:28 +0200

    Adding failing test

--- Python/TirePressureMonitoringSystem/test_tire_pressure_monitoring.py
+++ Python/TirePressureMonitoringSystem/test_tire_pressure_monitoring.py
@@ -7,3 +7,9 @@ class AlarmTest(unittest.TestCase):
     def test_alarm_is_off_by_default(self):
         alarm = Alarm()
         assert not alarm.is_alarm_on
+
+    def test_set_alarm(self):
+        alarm = Alarm()
+        actual = alarm.set()
+
+        self.assertTrue(actual.is_alarm_on)
