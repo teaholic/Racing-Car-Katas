6f6931f0747b13f891c9a00e558431014a4462aa
Chiara Barbagianni <chiara.barbagianni@agilelab.it>
2023-06-14 09:06:20 +0200

    Refactored HtmlPagesConverter

--- Python/TextConverter/html_pages.py
+++ Python/TextConverter/html_pages.py
@@ -5,14 +5,15 @@ import html as html_converter
 
 # for Python 2 uncomment this line
 #import cgi as html_converter
+from typing import List
 
-class HtmlPagesConverter:
 
-    def __init__(self, filename):
-        """Read the file and note the positions of the page breaks so we can access them quickly"""
-        self.filename = filename
-        self.breaks = [0]
-        with open(self.filename, "r", encoding="UTF-8") as f:
+class FileRepository:
+
+    """Read the file and note the positions of the page breaks so we can access them quickly"""
+    def _get_breaks(self, filename) -> List[int]:
+        breaks: List[int] = [0]
+        with open(filename, "r", encoding="UTF-8") as f:
             while True:
                 line = f.readline()
                 if not line:
@@ -20,22 +21,36 @@ class HtmlPagesConverter:
                 line = line.rstrip()
                 if "PAGE_BREAK" in line:
                     page_break_position = f.tell()
-                    self.breaks.append(f.tell())
-            self.breaks.append(f.tell())                
+                    breaks.append(f.tell())
+            breaks.append(f.tell())
+            return breaks
 
-    def get_html_page(self, page):
-        """Return html page with the given number (zero indexed)"""
-        page_start = self.breaks[page]
-        page_end = self.breaks[page+1]
-        html = ""
-        with open(self.filename, "r", encoding="UTF-8") as f:
+    def get_lines(self, filename, page) -> List[str]:
+        lines = []
+        breaks = self._get_breaks(filename)
+        page_start = breaks[page]
+        page_end = breaks[page + 1]
+        with open(filename, "r", encoding="UTF-8") as f:
             f.seek(page_start)
             while f.tell() != page_end:
                 line = f.readline()
                 line = line.rstrip()
                 if "PAGE_BREAK" in line:
                     continue
-                html += html_converter.escape(line, quote=True)
-                html += "<br />"
+                lines.append(line)
+        return lines
+
+
+class HtmlPagesConverter:
+    def __init__(self, repository: FileRepository):
+        self.repository = repository
+
+    def get_html_page(self, filename: str, page):
+        """Return html page with the given number (zero indexed)"""
+        html = ""
+        lines = self.repository.get_lines(filename=filename, page=page)
+        for line in lines:
+            html += html_converter.escape(line, quote=True)
+            html += "<br />"
         return html
 
--- Python/TextConverter/test_html_pages.py
+++ Python/TextConverter/test_html_pages.py
@@ -1,10 +1,15 @@
 from unittest import TestCase
+from unittest.mock import MagicMock
+
+from html_pages import HtmlPagesConverter
 
 
 class TestHtmlPagesConverter(TestCase):
 
     def test_convert_to_html(self):
-        self.assertTrue(False)
+        repository = MagicMock()
+        repository.get_lines.return_value = ["Hello world", "How do you do?"]
 
-    def add_pagination(self):
-        self.assertTrue(False)
+        expected = "Hello world<br />How do you do?<br />"""
+        actual = HtmlPagesConverter(repository).get_html_page('f', 1)
+        self.assertEqual(actual, expected)
