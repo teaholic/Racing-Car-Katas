8358450f4e77722a627df79fb576af58578cac2f
Chiara Barbagianni <chiara.barbagianni@agilelab.it>
2023-06-14 08:37:09 +0200

    Adding failing tests

---
+++ Python/TextConverter/test_html_pages.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+
+class TestHtmlPagesConverter(TestCase):
+
+    def test_convert_to_html(self):
+        self.assertTrue(False)
+
+    def add_pagination(self):
+        self.assertTrue(False)
